7d222e10530d73082d6f4beee372a99f374e7d2d
Kishore868 <kishorechandra.cs23@bmsce.ac.in>
2026-01-22 09:05:06 +0530

    Revert "Implemented update password and name functionality"
    This reverts commit 2296f4c494e10af7572ddb3dd0cd03fbe9700320.

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,6 @@
 import { APIResponse } from "../utils/APIResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { changeUserPassword } from "../services/user.services.js";
-import { updateUserFullName } from "../services/user.services.js";
+
 const getMe = asyncHandler(async (req, res) => {
   return res.status(200).json(
     new APIResponse({
@@ -15,35 +14,5 @@ const getMe = asyncHandler(async (req, res) => {
     })
   );
 });
-const changePassword = asyncHandler(async (req, res) => {
-  const { currentPassword, newPassword } = req.body;
 
-  await changeUserPassword({
-    userId: req.user._id,
-    currentPassword,
-    newPassword,
-  });
-
-  return res.status(200).json(
-    new APIResponse({
-      message: "Password changed successfully. Please login again.",
-    })
-  );
-});
-const updateFullName = asyncHandler(async (req, res) => {
-  const { fullName } = req.body;
-
-  const updatedUser = await updateUserFullName({
-    userId: req.user._id,
-    fullName,
-  });
-
-  return res.status(200).json(
-    new APIResponse({
-      message: "Full name updated successfully",
-      data: updatedUser,
-    })
-  );
-});
-
-export { getMe,changePassword,updateFullName };
+export { getMe };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,9 @@
 import { Router } from "express";
-import { getMe, changePassword } from "../controllers/user.controllers.js";
+import { getMe } from "../controllers/user.controllers.js";
 import { requireAuth } from "../middlewares/auth.middlewares.js";
-import { validate } from "../middlewares/validate.middlewares.js";
-import { changePasswordSchema, updateFullNameSchema, } from "../validators/user.schemas.js";
-import { updateFullName } from "../controllers/user.controllers.js";
+
 const router = Router();
 
 router.get("/me", requireAuth, getMe);
-router.patch(
-    "/change-password",
-    requireAuth,
-    validate(changePasswordSchema),
-    changePassword
-  );
-  router.patch(
-    "/update-name",
-    requireAuth,
-    validate(updateFullNameSchema),
-    updateFullName
-  );
 
 export default router;
